db0dfc7b89075f2aa6db5b011406fc691d7cf827
Alejacks <alejandrolopez2048@gmail.com>
2025-11-03 08:55:46 +0100

    feat: Esqueleto del proyecto creado sin tener en cuenta testing. Se añade .gitignore y README.md

---
+++ src/main/java/org/cuatrovientos/psp/tamago/App.java
@@ -0,0 +1,13 @@
+package org.cuatrovientos.psp.tamago;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/org/cuatrovientos/psp/tamago/Tamagotchi.java
@@ -0,0 +1,4 @@
+package org.cuatrovientos.psp.tamago;
+
+public class Tamagotchi {
+}
