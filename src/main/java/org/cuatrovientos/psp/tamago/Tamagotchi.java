e5aac2fd201717931021ed1b03f51292f668aea2
Alejacks <alejandrolopez2048@gmail.com>
2025-11-04 10:06:18 +0100

    feat: Forma de Tamagotchi.java. Todavía no tiene lógica ni run() implementado, nada más que un constructor

--- src/main/java/org/cuatrovientos/psp/tamago/Tamagotchi.java
+++ src/main/java/org/cuatrovientos/psp/tamago/Tamagotchi.java
@@ -1,4 +1,38 @@
 package org.cuatrovientos.psp.tamago;
 
-public class Tamagotchi {
+import java.util.Random;
+
+public class Tamagotchi implements Runnable {
+
+    private static final Random RANDOM = new Random();
+    
+    private String nombre;
+
+    private int tiempoParaComer;
+    private Estado estado;
+
+    public long nacimiento;
+
+    public Tamagotchi(String nombre) {
+        this.nombre = nombre;
+        this.tiempoParaComer = RANDOM.nextInt(5);
+        this.nacimiento = System.currentTimeMillis();
+        this.estado = Estado.VIVO;
+    }
+
+    @Override
+    public void run() {}
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    private enum Estado {
+        VIVO, ASEANDO, JUGANDO, COMIENDO, MUERTO
+    }
+    
 }
